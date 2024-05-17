3dcbb6c26a2b94c88029f0b73f353954d9159443
Serdyukov Alexandr <serdyukov2018@bk.ru>
2024-05-17 15:12:53 +0500

    WPFCalculatorLab6

---
+++ WpfCalc/Calculate_RPN.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCalc
+{
+    public class Calculate_RPN
+    {
+        public static double CalculateRPN(string rpn)
+        {
+            string[] tokens = rpn.Split(' ');
+            Stack<double> numberStack = new Stack<double>();
+
+            foreach (string token in tokens)
+            {
+                if (double.TryParse(token, out double number))
+                {
+                    numberStack.Push(number);
+                }
+                else if (numberStack.Count >= 2)
+                {
+                    double operand2 = numberStack.Pop();
+                    double operand1 = numberStack.Pop();
+
+                    switch (token)
+                    {
+                        case "+":
+                            numberStack.Push(operand1 + operand2);
+                            break;
+                        case "-":
+                            numberStack.Push(operand1 - operand2);
+                            break;
+                        case "*":
+                            numberStack.Push(operand1 * operand2);
+                            break;
+                        case "/":
+                            numberStack.Push(operand1 / operand2);
+                            break;
+                    }
+                }
+            }
+
+            return numberStack.Count > 0 ? numberStack.Peek() : 0;
+        }
+    }
+}
---
+++ WpfCalc/Infix_To_RPN.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCalc
+{
+    public class Infix_To_RPN
+    {
+        public static string InfixToRPN(string infix)
+        {
+            string[] tokens = infix.Split(' ');
+            string rpn = "";
+            Stack<string> operatorStack = new Stack<string>();
+
+            Dictionary<string, int> precedence = new Dictionary<string, int>
+            {
+                { "+", 1 },
+                { "-", 1 },
+                { "*", 2 },
+                { "/", 2 }
+            };
+
+            foreach (string token in tokens)
+            {
+                if (double.TryParse(token, out double number))
+                {
+                    rpn += number + " ";
+                }
+                else if (precedence.ContainsKey(token))
+                {
+                    while (operatorStack.Count > 0 &&
+                           precedence.ContainsKey(operatorStack.Peek()) &&
+                           precedence[token] <= precedence[operatorStack.Peek()])
+                    {
+                        rpn += operatorStack.Pop() + " ";
+                    }
+                    operatorStack.Push(token);
+                }
+                else if (token == "(")
+                {
+                    operatorStack.Push(token);
+                }
+                else if (token == ")")
+                {
+                    while (operatorStack.Count > 0 && operatorStack.Peek() != "(")
+                    {
+                        rpn += operatorStack.Pop() + " ";
+                    }
+                    if (operatorStack.Count > 0 && operatorStack.Peek() == "(")
+                    {
+                        operatorStack.Pop();
+                    }
+                }
+            }
+
+            while (operatorStack.Count > 0)
+            {
+                rpn += operatorStack.Pop() + " ";
+            }
+
+            return rpn;
+        }
+    }
+}
